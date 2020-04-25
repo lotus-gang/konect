3675b1dcc33ab1b06162b57002b2ddf279d91703
hung arima <tronghung199@gmail.com>
2020-04-25 22:34:37 +0700

    done bottom-nav

--- src/pages/Suppliers.js
+++ src/pages/Suppliers.js
@@ -16,8 +16,22 @@ const Suppliers = () => {
       close_hour: 18,
       location: {
         lat: 1.3265611,
-        lng: 103.6823505
-      }
+        lng: 103.6823505,
+      },
+    },
+    {
+      id: "s3",
+      name: "DBrandt Oilfield Services",
+      imageUrl:
+        "https://a.c-dn.net/b/4uot3B/headline_shutterstock_243762007.jpg",
+      address: "8 Sixth Lok Yang Rd, Singapore 628106",
+      mobile: 68621169,
+      open_hour: 9,
+      close_hour: 18,
+      location: {
+        lat: 1.3265611,
+        lng: 103.6823505,
+      },
     },
     {
       id: "s2",
@@ -30,9 +44,23 @@ const Suppliers = () => {
       close_hour: 18,
       location: {
         lat: 1.3265611,
-        lng: 103.6823505
-      }
-    }
+        lng: 103.6823505,
+      },
+    },
+    {
+      id: "s4",
+      name: "DBrandt Oilfield Services",
+      imageUrl:
+        "https://a.c-dn.net/b/4uot3B/headline_shutterstock_243762007.jpg",
+      address: "8 Sixth Lok Yang Rd, Singapore 628106",
+      mobile: 68621169,
+      open_hour: 9,
+      close_hour: 18,
+      location: {
+        lat: 1.3265611,
+        lng: 103.6823505,
+      },
+    },
   ];
   return <SupplyList items={SUPPLIERS} />;
 };
