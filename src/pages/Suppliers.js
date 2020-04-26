b291f1d7db015a4b4260663775d5ca17653d3d74
hung arima <tronghung199@gmail.com>
2020-04-26 10:22:27 +0700

    minor bugs

--- src/pages/Suppliers.js
+++ src/pages/Suppliers.js
@@ -25,34 +25,7 @@ const Suppliers = () => {
       })
       .catch(console.error);
   }, []);
-  const SUPPLIERS = [
-    {
-      id: "s1",
-      name: "Giang Garment Factory",
-      imageUrl:
-        "https://a.c-dn.net/b/4uot3B/headline_shutterstock_243762007.jpg",
-      address: "8 Sixth Lok Yang Rd, Singapore 628106",
-      mobile: 68621169,
-      available: false,
-      location: {
-        lat: 1.3265611,
-        lng: 103.6823505,
-      },
-    },
-    {
-      id: "s2",
-      name: "DBrandt Oilfield Services",
-      imageUrl:
-        "https://a.c-dn.net/b/4uot3B/headline_shutterstock_243762007.jpg",
-      address: "8 Sixth Lok Yang Rd, Singapore 628106",
-      mobile: 68621169,
-      available: true,
-      location: {
-        lat: 1.3265611,
-        lng: 103.6823505,
-      },
-    },
-  ];
+
   return data.length != 0 && <SupplyList items={data} />;
 };
 
